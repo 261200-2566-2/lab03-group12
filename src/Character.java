b99b187010cec071dcf19fedbada5d45df14a3fe
pubestpubest <pubestpubest@gmail.com>
2023-11-26 02:39:24 +0700

    all done ps. don't insert weird input please

--- src/Character.java
+++ src/Character.java
@@ -1,78 +1,100 @@
 import java.util.Objects;
 
 public class Character {
-    private String name;
-    private int level,maxHP,maxMP,maxSp;
-    private double hp,mp;
+    //fields
+    private final String name;
+    private  int level;
+    private double maxHP,maxMP,maxSp;
+    private double hp;
+    private final double mp;
+    private double sp;
+    private final double baseSp;
     private Equipment mainHand;
     private boolean alive=true;
-    public Character(String name,int level,int speed){
-        this.name = name;
-        this.level = level;
-        this.maxSp = speed;
-        maxHP = 100+10*level;
-        maxMP = 50+2*level;
-        hp=maxHP;
-        mp=maxMP;
-        mainHand=null;
-    }
+    //constructors
     public Character(String name,int level,int speed,Equipment weapon){
         this.name = name;
         this.level = level;
-        this.maxSp = speed;
-        maxHP = 100+10*level;
-        maxMP = 50+2*level;
+        this.baseSp = speed;
+        mainHand = new Equipment(weapon.getName(), weapon.getLevel(), weapon.getType());
+        updateStats();
         hp=maxHP;
         mp=maxMP;
-        mainHand = new Equipment(weapon.getName(), weapon.getLevel(), weapon.getType());
     }
-//    public  int getMaxHP(){return  maxHP;}
-//    public int getMaxMP() {return maxMP;}
-//    public int getLevel() {return level;}
-//    public int getHp() {return hp;}
-//    public int getMp() {return mp;}
-
-
+    public Character(String name,int level,int speed){
+        this(name,level,speed,new Equipment("Bare Hand",0,"bareHand"));
+    }
+    //public methods
     public String getName() {
         return name;
-    }
-
-    //public Equipment getMainHand() {return mainHand;}
-
+    }           //getter
+    public void setMainHand(Equipment mainHand) {
+        this.mainHand = new Equipment(mainHand.getName(), mainHand.getLevel(), mainHand.getType());
+        updateStats();
+        statDisplay();
+    }  //setter
+    public void heal() {
+        System.out.println(name+"was healed to full hp\n");
+        hp = maxHP;
+    }                           //heal
+    public void removeEquip(){
+        this.mainHand = new Equipment("Bare Hand",0,"bareHand");
+        updateStats();
+        statDisplay();
+    }                     //remove equipment
+    public void mainHandUpLevel(){
+        mainHandUpLevel(1);
+    }   //increase main hand weapon's level
+    public void mainHandUpLevel(int lvl){
+        mainHand.upLevel(lvl);
+        updateStats();
+    }          //method overloading
     public void upLevel(){
-        level++;
-        maxHP = 100+10*level;
-        maxMP = 50+2*level;
-    }
+        upLevel(1);
+    }          //increase character's level
+    public void upLevel(int lvl){
+        level+=lvl;
+        updateStats();
+        System.out.println(name+" LEVEL UP");
+        statDisplay();
+    }                  //method overloading
     public void attack(Character target){
         //no weapon you only deal 1 dmg
-        double dmg;
-        System.out.println(name+" Attack "+target.getName()+(mainHand!=null?" with "+mainHand.getName():""));
-        if(mainHand==null){
+        double dmg=5;
+        System.out.println(name+" Attack "+target.getName()+" with "+mainHand.getName()+" Lvl."+mainHand.getLevel());
+        if(Objects.equals(mainHand.getName(), "bareHand")){
             dmg=1;
-            target.beingAttack(dmg);
         }else if (Objects.equals(mainHand.getType(), "sword")){
             dmg=mainHand.getStats();
-            target.beingAttack(dmg);
         }
-    }
+        target.beingAttack(dmg);
+    }          //attack
+    public void statDisplay(){
+        System.out.println(name+"'s Stats"+"\nStatus : "+(alive?"ALIVE":"DEAD"));
+        System.out.println("MainHand\t: "+mainHand.getName());
+        System.out.println("Level\t: "+level);
+        System.out.println("Health\t: "+String.format("%.1f",hp)+"/"+String.format("%.1f",maxHP));
+        System.out.println("Mana\t: "+String.format("%.1f",mp)+"/"+String.format("%.1f",maxMP));
+        System.out.println("Speed\t: "+String.format("%.1f",sp)+"/"+String.format("%.1f",maxSp)+"\n");
+    }                     //show character's stats
+    //private methods
+    private void updateStats(){
+        maxHP = 100+10*level;
+        maxMP = 50+2*level;
+        maxSp = baseSp*(1+0.03*level);
+        if(!Objects.equals(mainHand.getType(), "bareHand")) sp = maxSp - baseSp*(mainHand.getSpDec());
+        else    sp = maxSp;
+    }                    //update character's stats
     private void beingAttack(double dmg){
-        if(mainHand!=null&&Objects.equals(mainHand.getType(), "shield")){
-            System.out.println(name+" block it with "+mainHand.getName());
+        if(Objects.equals(mainHand.getType(), "shield")){
+            System.out.println(name+" block it with "+mainHand.getName()+" Lvl."+mainHand.getLevel());
             dmg-=mainHand.getStats();
             if(dmg<0)   dmg = 0;
         }
-        System.out.println("Damage : "+dmg);
+        System.out.println("Damage : "+String.format("%.1f",dmg));
         hp-=dmg;
         if(hp<=0)   alive=false;
-        if(!alive) System.out.println(name+"has been slain.");
-        else System.out.println(name+"'s health : "+hp+"/"+maxHP+"\n");
-    }
-    public void statDisplay(){
-        System.out.println(name+"\nStatus : "+(alive?"ALIVE":"DEAD"));
-        if(mainHand!=null)    System.out.println("MainHand : "+mainHand.getName());
-        System.out.println("Level\t: "+level);
-        System.out.println("Health\t: "+hp+"/"+maxHP);
-        System.out.println("Mana\t: "+mp+"/"+maxMP+"\n");
-    }
+        if(!alive) System.out.println(name+"has been slain.\n");
+        else System.out.println(name+"'s health : "+String.format("%.1f",hp)+"/"+String.format("%.1f",maxHP)+"\n");
+    }          //being attack
 }
--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,44 @@ public class Main {
     public static void main(String[] args) {
         Equipment sword = new Equipment("Great Sword",50,"sword");
         Equipment shield = new Equipment("Great Shield",30,"shield");
+        //a character might have a sword equipped, or not, and might have a shield equipped, or not
         Character a1 = new Character("Warrior",1,10,sword);
         Character a2 = new Character("Mage",1,5);
         Character b1 = new Character("Goblin",1,20,shield);
-//        a1.statDisplay();
-//        a2.statDisplay();
-//        b1.statDisplay();
+        //an RPG character has a level, which determines maximum HP and mana
         a1.statDisplay();
+        //level can be upped
+        a1.upLevel();
+        //a character could be attacked by a sword, but can lessen damage with a shield
+        //sword attack bare hand
         a1.attack(a2);
+        //sword attack shield
         a1.attack(b1);
+        //bare hand attack sword
+        a2.attack(a1);
+        //shield attack sword
+        b1.attack(a1);
+        //equipping any of these will reduce character's run speed
+        //equipment has an effect on character's run speed
+        a1.statDisplay();
+        a1.removeEquip();
+        a1.setMainHand(shield);
+        a1.setMainHand(sword);
+        //a sword has a level that increases damage dealt to a character and affects character's run speed
+        a1.mainHandUpLevel(9);
+        a1.mainHandUpLevel();
+        a1.statDisplay();
+        a1.attack(b1);
+        //a shield has a level that decreases damage received by a character and affects character's run speed
+        b1.heal();
+        b1.statDisplay();
+        a1.attack(b1);
+        b1.heal();
+        b1.mainHandUpLevel(200);
+        b1.statDisplay();
+        a1.attack(b1);
+        //how and where to store data           object's private field that can't access from client's code
+        //what operations are needed            a lots go see each classes
+        //how and where to compute information  in private method ? maybe
     }
 }
